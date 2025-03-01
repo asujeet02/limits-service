5a4f715c0de27a7be526e8f73f62ea09a6a7d8b4
Sujeet Kumar <asujeet02@gmail.com>
2025-03-01 10:52:20 +0530

    Bootstrap Properties configured and dependencies added.

--- limits-service/src/main/java/com/cg/microservices/limits_service/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/cg/microservices/limits_service/controller/LimitsConfigurationController.java
@@ -4,17 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.microservices.limits_service.bean.Limits;
+import com.cg.microservices.limits_service.bean.LimitConfiguration;
 import com.cg.microservices.limits_service.configuration.Configuration;
 
 @RestController
-public class LimitsController {
-
+public class LimitsConfigurationController {
+	
 	@Autowired
 	private Configuration configuration;
 
-	/*
-	 * @GetMapping("/limits") public Limits retrieveLimits() { return new
-	 * Limits(configuration.getMinimum(), configuration.getMaximum()); }
-	 */
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+
 }
